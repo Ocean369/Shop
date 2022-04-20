b067743aacf0dfdc9395ff854a8be9805fe5baa9
Anastasia Kibalka <ledix369@gmail.com>
2022-04-20 23:14:55 +0300

    wednesday update, add  filter menu

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 let btn = document.querySelector('.header-menu_catalog');
 let menu = document.querySelector('.menu');
-let btnclose = document.querySelector('.close');
+let btnClose = document.querySelector('.close');
+let filterActive = document.querySelector('.filter');
+let btnFilter = document.querySelector('.basic');
+let categoryActive = document.querySelector('.categoryMenu');
+let brandActive = document.querySelector('.brandMenu');
+let designerActive = document.querySelector('.designerMenu');
+let menuItems = document.querySelector('.menuItems');
 
 btn.onclick = function () {
     menu.classList.toggle("menu-active");
 }
 
-btnclose.onclick = function () {
+btnClose.onclick = function () {
     menu.classList.remove("menu-active");
+}
+
+btnFilter.onclick = function () {
+    filterActive.classList.toggle("filter-active");
+}
+
+categoryActive.onclick = function () {
+    let title = categoryActive.querySelector(".title");
+    title.classList.toggle("active");
+    menuItems.classList.toggle('show');
 }
